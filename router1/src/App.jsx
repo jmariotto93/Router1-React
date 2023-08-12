97d1bfd78c4f1c055b7a94dfee7cb48f0e28e25d
José Mariotto <josemarioto@gmail.com>
2023-08-12 20:45:11 -0300

    create Error404

--- router1/src/App.jsx
+++ router1/src/App.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Home from './Home'
+import Sobre from './Sobre';
+import Error404 from './Error404';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='sobre' element={<Sobre />} />
+        <Route path='*' element={<Error404 />} />
       </Routes>
     </BrowserRouter>
   );
