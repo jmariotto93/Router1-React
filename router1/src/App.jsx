448f2b07ddc318c148b37da21003583124ee872a
José Mariotto <josemarioto@gmail.com>
2023-08-12 21:44:25 -0300

    use o Link

--- router1/src/App.jsx
+++ router1/src/App.jsx
@@ -2,14 +2,18 @@ import React from 'react'
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Home from './Home'
 import Sobre from './Sobre';
+import Login from './Login';
 import Error404 from './Error404';
+import Header from './Header';
 
 function App() {
   return (
     <BrowserRouter>
+    <Header />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='sobre' element={<Sobre />} />
+        <Route path='login' element={<Login />} />
         <Route path='*' element={<Error404 />} />
       </Routes>
     </BrowserRouter>
