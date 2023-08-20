5047a14d284b04ad4bcbc7c8a31f40f4f5d1037f
José Mariotto <josemarioto@gmail.com>
2023-08-19 22:36:20 -0300

    import react-helmet

--- router1/src/Head.jsx
+++ router1/src/Head.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 const Head = (props) => {
   React.useEffect(() => {
+    document.title = props.title;
+    document.querySelector('meta[name="description"]').setAttribute('content', props.description);
     console.log(props)
-  }, [props])
+  }, [props]);
   return (
     <>
     </>
--- router1/src/Sobre.jsx
+++ router1/src/Sobre.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import Head from './Head'
+import {Helmet} from 'react-helmet'
 
 const Sobre = () => {
   return (
     <div>
+      {/*<Head title="Sobre" description="Essa é a descrição da Sobre" /> */}
+      <Helmet>
+        <title>Sobre</title>
+        <meta name="description" content="Essa é a descrição da Sobre" />
+      </Helmet>
         <h1>
             Sobre
         </h1>
